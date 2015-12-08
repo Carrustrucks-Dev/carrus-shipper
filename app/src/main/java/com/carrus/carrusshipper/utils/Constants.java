5cc2e0018cf07340940d3583f93b925d5b6cae42
Sunny Saini <sunny.saini@clicklabs.in>
2015-12-08 17:20:32 +0530

    login and logout api implemented

--- app/src/main/java/com/carrus/carrusshipper/utils/Constants.java
+++ app/src/main/java/com/carrus/carrusshipper/utils/Constants.java
@@ -5,9 +5,9 @@ package com.carrus.carrusshipper.utils;
  */
 public class Constants {
 
-    public static String ROOT ="http://52.25.204.93:8080";
+//    public static String ROOT ="http://52.25.204.93:8080";
 //    public static String ROOT ="http://52.25.204.93:3005";
-//    public static String ROOT ="http://52.25.204.93:3006";
+    public static String ROOT ="http://52.25.204.93:3006";
     public static final String GOOGLE_URL ="http://maps.googleapis.com";
     public static final String SENDER_ID="57143499411";
     public static final String DEVICE_TYPE="ANDROID";
