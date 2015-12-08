87c2ee565045381e29e19edd745a4e3b0b31732b
Sunny Saini <sunny.saini@clicklabs.in>
2015-12-08 12:57:17 +0530

    new server added

--- app/src/main/java/com/carrus/carrusshipper/utils/Constants.java
+++ app/src/main/java/com/carrus/carrusshipper/utils/Constants.java
@@ -7,6 +7,7 @@ public class Constants {
 
     public static String ROOT ="http://52.25.204.93:8080";
 //    public static String ROOT ="http://52.25.204.93:3005";
+//    public static String ROOT ="http://52.25.204.93:3006";
     public static final String GOOGLE_URL ="http://maps.googleapis.com";
     public static final String SENDER_ID="57143499411";
     public static final String DEVICE_TYPE="ANDROID";
