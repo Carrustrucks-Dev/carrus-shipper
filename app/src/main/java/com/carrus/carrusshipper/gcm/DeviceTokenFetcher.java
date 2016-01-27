696d8ea6e9cc4bafa20797a4dbe1910aaf350e8a
Sunny Saini <sunny.saini@clicklabs.in>
2016-01-27 17:34:51 +0530

    bugs fixed

--- app/src/main/java/com/carrus/carrusshipper/gcm/DeviceTokenFetcher.java
+++ app/src/main/java/com/carrus/carrusshipper/gcm/DeviceTokenFetcher.java
@@ -64,8 +64,8 @@ public class DeviceTokenFetcher extends AsyncTask<String, Void, String> {
     protected String doInBackground(String... params) {
 
         try {
-            if(activity!=null)
-            return GoogleCloudMessaging.getInstance(activity).register(params[0]);
+            if (activity != null && GoogleCloudMessaging.getInstance(activity) != null)
+                return GoogleCloudMessaging.getInstance(activity).register(params[0]);
         } catch (IOException e) {
             e.printStackTrace();
         }
